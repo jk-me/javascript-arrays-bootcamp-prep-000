03ad02c849f863486ef9365f20df0a4434de21ca
Jenny Kam <jkam17@gmail.com>
2018-02-26 01:21:18 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(a,b){
+  var c = [b,...a]
+  return c
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b){
+  a.unshift(b)
+  return a
+}
+
+function addElementToEndOfArray(a,b){
+  var c = [...a,b]
+  return c
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  a.push(b)
+  return a
+}
+
+
+
+
+
+
+
+
+
+
